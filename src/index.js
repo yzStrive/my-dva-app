0f627899d69904c676a93bc071b67992f7121647
yzstrive <yzStrive@gmail.com>
2018-04-01 23:19:39 +0800

    [A]: init project reference of ant-design-pro

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import dva from 'dva';
-import './index.css';
-
+import dva from "dva"
+import "./index.less"
+import createLoading from 'dva-loading'
 // 1. Initialize
-const app = dva();
+const app = dva()
 
 // 2. Plugins
-// app.use({});
+app.use(createLoading());
 
 // 3. Model
 // app.model(require('./models/example').default);
 
 // 4. Router
-app.router(require('./router').default);
+app.router(require("./router").default)
 
 // 5. Start
-app.start('#root');
+app.start("#root")
